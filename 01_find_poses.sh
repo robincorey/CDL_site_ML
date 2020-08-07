98a4badab59eed6419dcba02a57b929cfccf0a02
robincorey <robin.corey@gmail.com>
2020-08-07 11:30:48 +0100

    changes to FEP analysis script

--- 01_find_poses.sh
+++ 01_find_poses.sh
@@ -81,7 +81,6 @@ rm -f site_info/chosen.txt
 for pdb in 1FFT 1FX8 1KF6 1KPK 1NEK 5OQT 4JR9 2HI7 3O7P 3ZE3 1ZCD 5OC0 1PV6 3OB6 5MRW 5AZC 1Q16 2QFI 2IC8 1RC2 1IWG 2WSX 5JWY 3B5D 3DHW 1PW4 4Q65 4DJI 2R6G 4GD3 5ZUG 6AL2 1L7V 4IU8 4KX6 3QE7 5SV0 1U77 5AJI 4ZP0 3K07 1KQF
 do
 	mkdir -p $CD/Sites_for_ML/$pdb
-	# this is wrong, of course
 	site_dir=$CD/Sites_new/$pdb/lipid_interactions/Interaction_CARD/Binding_Sites_CARD
 	# gets the range of sites
 	total=`(cd $site_dir/Binding_Poses && ls *gro) | tr -d [[:alpha:]]. | awk -F '_' '{print $1}' | sort -u | wc -l`
--- 04_analyse_FEP.sh
+++ 04_analyse_FEP.sh
@@ -1,26 +1,42 @@
 #!/bin/bash
 
-DATA_DIR=/sansom/s156a/bioc1535/EC_MEMPROT/5us_analyses/FEP_data/Data
+DATA_DIR=/sansom/s156a/bioc1535/EC_MEMPROT/5us_analyses
 
 analyse_FEP () {
-/sansom/s137/bioc1535/.local/bin/alchemical_analysis -d $1 -o $1/Analysis_$2 -q "_${2}.xvg" -p "md_" -i 100000 -t 323 -s 2000
+mkdir -p $1/Analysis_$2
+if [[ ! -f $1/Analysis_$2/results.txt ]]
+then
+	/sansom/s137/bioc1535/.local/bin/alchemical_analysis -d $1 -o $1/Analysis_$2 -q "_${2}.xvg" -p "md_" -i 100000 -t 323 -s 2000 
+fi
+tail -n 1 $1/Analysis_$2/results.txt | awk '{print $(NF-2)}' >> $DATA_DIR/FEP_data/energies/$3.txt
 }
 
-extract_info () {
-:
+get_other_values () {
+site_dir=$DATA_DIR/Sites_new/$1/lipid_interactions/Interaction_CARD/Binding_Sites_CARD
+read -r koff koff_err <<<$(grep -A10 "Binding site $2$" $site_dir/BindingSites_Info_CARD.txt | grep "BS koff Bootstrap" | awk '{print $4" "$6}') 
+occ=`grep -A10 "Binding site $2$" $site_dir/BindingSites_Info_CARD.txt | grep "BS Lipid Occupancy" | awk '{print $4}'`
+fep=`tail -n+2 $DATA_DIR/FEP_data/energies/$1_$2_$3.txt | awk '{sum+=$1} END {print sum/NR}'`
+fep_error=`tail -n+2 $DATA_DIR/FEP_data/energies/$1_$2_$3.txt | awk '{sum+=$1;a[NR]=$1}END{for(i in a)y+=(a[i]-(sum/NR))^2;print sqrt(y/(NR-1))}'`
+echo -e "$fep $fep_error $koff $koff_err $occ 0 " >> $DATA_DIR/FEP_data/energies/comparison.txt 
 }
 
-for i in `ls $DATA_DIR`
+mkdir -p $DATA_DIR/FEP_data/energies
+echo "dur occ fep" > $DATA_DIR/FEP_data/energies/comparison.txt
+
+for i in `ls $DATA_DIR/FEP_data/Data`
 do
-	fep=$DATA_DIR/$i/Data/
-	for rep in {1..10}
-	do
-		mkdir -p $fep/Analysis_$rep/
-		while [ `top -n1 | grep alchem | wc -l` -gt 4 ]
-		do 
-			sleep 5
+	fep=$DATA_DIR/FEP_data/Data/$i/Data
+	if [[ -d $fep ]]
+	then
+		echo $i > $DATA_DIR/FEP_data/energies/$i.txt
+		for rep in {1..10}
+		do
+			while [ `top -n1 | grep alchem | wc -l` -gt 4 ]
+			do
+				sleep 5
+			done		
+			analyse_FEP $fep $rep $i >& $fep/out &
 		done
-		analyse_FEP $fep $rep >& $fep/out &
-	done
-	
+		get_other_values `echo $i | cut -f1 -d'_'` `echo $i | cut -f2 -d'_'` `echo $i | cut -f3 -d'_'`
+	fi
 done
