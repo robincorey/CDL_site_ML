fe352e79f309a529bb1837e683f3b2af05b9999e
robincorey <robin.corey@gmail.com>
2023-01-06 11:56:56 +0000

    starting training data script

---
+++ 06_training_data.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# This needs to run within a singularity container 
+# Still a work in progress
+
+
+# training data_dir must be defined here
+# this needs to include atoms types - still being developed
+
+training_data_dir = ./data/
+
+cd ${training_data_dir}
+
