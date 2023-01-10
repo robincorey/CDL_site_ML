fadc4040769b557fa6d1c4cb9320b22d1431711e
robincorey <robin.corey@gmail.com>
2023-01-10 11:31:19 +0000

    getting RDKit script working

--- test_RDKit/analyse_rdkit.py
+++ test_RDKit/analyse_rdkit.py
@@ -3,6 +3,7 @@ import argparse
 from rdkit.Chem import AllChem
 
 # script to convert a list of molecules in SMILES format to SDF 
+# works, could do with some more arg options
 
 def readSmiles(input):
 	suppl = Chem.SmilesMolSupplier(input)
@@ -11,7 +12,7 @@ def readSmiles(input):
 		if mol:
 			Chem.SanitizeMol(mol)
 			smiles_list.append(mol)
-	print("Read {}/{} molecules".format(len(smiles_list), len(suppl)))
+	print("Read %s/%s molecules" % (len(smiles_list), len(suppl)))
 	return smiles_list
 
 def Smilestomodel(mol):
@@ -22,14 +23,18 @@ def Smilestomodel(mol):
 	return mol_H
 
 def write_SDF(all_models):
-	for mol in all_models:
-		
+	outfile = Chem.SDWriter(args.output)
+	#for mol in all_models:
+	outfile.write(mol)
+	print("Wrote %s/%s molecules to %s" % (len(all_models), len(smiles_list), args.output))
 
 if __name__ == '__main__':
 	# define arguents here. Add more as needed.
 	parser = argparse.ArgumentParser()
-	group_input = parser.add_argument_group('INPUT arguments')
+	group_input = parser.add_argument_group('input')
 	group_input.add_argument("-i", "--input", metavar='filename', type=str, required=True, help="name of SMILES file")
+	group_output = parser.add_argument_group('outputs')
+	group_output.add_argument("-o", "--output", metavar='filename', required=True, type=str, help="name of output SDF file")
 	
 	# parse arguments
 	args = parser.parse_args()
